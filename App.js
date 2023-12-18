c0e7fd553c3e2e807ba3cbef79d133d04071bd09
s24579 <s24579@pjwstk.edu.pl>
2023-12-18 13:54:07 +0100

    Revert "commit"
    This reverts commit 15cdd3ec6302c2ffcb0f14db6f88c9cb6c1a1ed5.

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< Updated upstream
 import HomeScreen from './screens/HomeScreen'; // Update the path
 
 export default function App() {
@@ -7,30 +6,3 @@ export default function App() {
       <HomeScreen />
   );
 }
-=======
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import Map from './pages/Map';
-import Events from './pages/Events';
-import Login from './pages/Login';
-
-const Stack = createStackNavigator();
-
-const App = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
-                <Stack.Screen name="Home" component={Home} />
-                <Stack.Screen name="Profile" component={Profile} />
-                <Stack.Screen name="Map" component={Map} />
-                <Stack.Screen name="Events" component={Events} />
-                <Stack.Screen name="Login" component={Login} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
-};
-
-export default App;
->>>>>>> Stashed changes
